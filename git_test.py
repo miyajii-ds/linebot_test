8110a5166fc6a8be50cc43de2fd040047e17cadc
miyajii-ds <75560193+miyajii-ds@users.noreply.github.com>
2020-12-09 22:30:49 +0900

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -1,22 +1,26 @@
-from flask import *
+from flask import Flask,request
 
 app = Flask(__name__)
 
-
-@app.route("/", methods=["GET", "POST"])
-def odd_even():
-    if request.method == "GET":
-        return """
-        下に整数を入力してください。奇数か偶数か判定します
-        <form action="/" method="POST">
-        <input name="num"></input>
-        </form>"""
-    else:
-        return """
-        {}は{}です！
-        <form action="/" method="POST">
-        <input name="num"></input>
-        </form>""".format(str(request.form["num"]), ["偶数", "奇数"][int(request.form["num"]) % 2])
-
+@app.route("/",methods=["GET","POST"])
+def hello():
+	if request.method =="GET":
+		return """整数入れて
+		<form action="/" method="POST">
+		<input name="num"></input>
+		</form>"""
+	else:
+		try
+			return """
+			{}は{}です
+			<form action="/" method="POST">
+			<input name="num"></input>
+			</form>""".format(str(request.form["num"]),["偶数","奇数"][int(request.form["num"])%2])
+		except:
+			return"""NG
+			<form action="/" method="POST">
+			<input name="num"></input>
+			</form>"""
+	
 if __name__ == "__main__":
-    app.run()
+	app.run()

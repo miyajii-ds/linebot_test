3d78198250bfe7e3c9381153833e7d844f5ffa7f
miyajii-ds <75560193+miyajii-ds@users.noreply.github.com>
2020-12-09 22:12:23 +0900

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -6,19 +6,19 @@ app = Flask(__name__)
 def hello():
 	if request.method == 'GET':
 		return '''整数入れて!!
-		<form action="/" method="POST>
+		<form action="/" method="POST">
 		<input name="num"></input>
 		</form>'''
 	else:
 		try:
 			return '''
 			{}は{}です
-			<form action="/" method="POST>
+			<form action="/" method="POST">
 			<input name="num"></input>
-			</form>'''.format(str(request.form["num"]),['偶数','奇数'][int(request.form['num'])%2)
+			</form>'''.format(str(request.form["num"]),['偶数','奇数'][int(request.form["num"])%2)
 		except:
 			return'''NG
-			<form action="/" method="POST>
+			<form action="/" method="POST">
 			<input name="num"></input>
 			</form>'''
 	

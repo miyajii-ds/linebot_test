1526fffadb366dc1f642f6bdef6a77df118b47e4
miyajii-ds <75560193+miyajii-ds@users.noreply.github.com>
2020-12-09 22:33:36 +0900

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def hello():
 	if request.method =="GET":
-		return """整数入れて
+		return """
+		整数入れて
 		<form action="/" method="POST">
 		<input name="num"></input>
 		</form>"""
@@ -17,7 +18,8 @@ def hello():
 			<input name="num"></input>
 			</form>""".format(str(request.form["num"]),["偶数","奇数"][int(request.form["num"])%2])
 		except:
-			return"""NG
+			return"""
+			NG
 			<form action="/" method="POST">
 			<input name="num"></input>
 			</form>"""

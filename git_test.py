79fd26932a6d2f65e837e912ae0e1dcaaf602a50
miyajii-ds <75560193+miyajii-ds@users.noreply.github.com>
2020-12-09 22:08:31 +0900

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route('/abcde')
+@app.route('abcde',methods='GET','POST')
 def hello():
-	return 'hello!!'
-
+	if request.method == 'GET':
+		return '''整数入れて!!
+		<form action="/" method="POST>
+		<input name="num"></input>
+		</form>'''
+	else:
+		try:
+			return '''
+			{}は{}です
+			<form action="/" method="POST>
+			<input name="num"></input>
+			</form>'''.format(str(request.form["num"]),['偶数','奇数'][int(request.form['num'])%2)
+		except:
+			return'''NG
+			<form action="/" method="POST>
+			<input name="num"></input>
+			</form>'''
+	
 if __name__ == '__main__':
 	app.run()

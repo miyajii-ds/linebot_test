0e124ff7636c748f53ff9b5c01e7eb9919475657
miyajii-ds <75560193+miyajii-ds@users.noreply.github.com>
2021-02-02 07:43:07 +0900

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -38,7 +38,7 @@ handler = WebhookHandler(CHANNEL_SECRET)
 def ifttt_webhoook(event_id):
 	ifttt_url = 'https://maker.ifttt.com/trigger/'+event_id+'/with/key/'+IFTTT_KEY
 	
-	response = requests.post(url)
+	response = requests.post(ifttt_url)
 
 
 

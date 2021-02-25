35d3b1757757531a15f4f1b715dcd015ff35cd66
miyajii-ds <75560193+miyajii-ds@users.noreply.github.com>
2021-02-25 12:35:58 +0900

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -52,7 +52,19 @@ def top_page():
 # ユーザがメッセージを送信したとき、この URL へアクセスが行われます。
 @app.route('/callback', methods=['POST'])
 def callback_post():
-  
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info('Request body: ' + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
     return 'OK'
 
 

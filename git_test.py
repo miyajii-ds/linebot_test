64b1e588ea1051a1785b376c747c3b41b9cf1de0
miyajii-ds <75560193+miyajii-ds@users.noreply.github.com>
2021-03-02 12:08:41 +0900

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -26,7 +26,7 @@ def top_page():
 
 
 # ユーザがメッセージを送信したとき、この URL へアクセスが行われます。
-@app.route('/callback', methods=['GET'])
+@app.route('/callback', methods=['POST'])
 def callback_post():
 	ifttt_webhoook(webhooks_test)
 	return 'OK'

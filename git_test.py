d10d9f21012e920d7cadc3f5fd6ddd6ca9d337ed
miyajii-ds <75560193+miyajii-ds@users.noreply.github.com>
2021-03-05 12:17:37 +0900

    Update git_test.py

--- git_test.py
+++ git_test.py
@@ -10,6 +10,49 @@ import os
 import requests
 import time
 
+from flask import Flask, request, abort
+
+# 環境変数取得のため。
+import os
+
+# ログを出力するため。
+#import logging
+#import sys
+
+import requests
+import time
+
+app = Flask(__name__)
+
+
+# 大事な情報は環境変数から取得。
+IFTTT_KEY = os.environ['IFTTT_KEY']
+flag = 0
+print(flag)
+
+def ifttt_webhoook(event_id):
+	ifttt_url = 'https://maker.ifttt.com/trigger/'+event_id+'/with/key/'+IFTTT_KEY
+	response = requests.post(ifttt_url)
+	print(response)
+
+# 必須ではないけれど、サーバに上がったとき確認するためにトップページを追加しておきます。
+@app.route('/')
+def top_page():
+	return 'Here is root page.'
+
+
+# ユーザがメッセージを送信したとき、この URL へアクセスが行われます。
+@app.route('/callback', methods=['POST'])
+def callback_post():
+	print('before')
+	ifttt_webhoook('webhooks_test')
+	flag = 3+4
+	print('flag:'+str(flag))
+	return 'OK'
+
+if __name__ == '__main__':
+	app.run()
+
 app = Flask(__name__)
 
 
